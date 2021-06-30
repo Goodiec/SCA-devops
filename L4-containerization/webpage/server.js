4a474405d3135bd336e720323ad5bc110f03ceb9
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-06-30 21:11:56 +0100

    fix

--- L4-containerization/webpage/server.js
+++ L4-containerization/webpage/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("This is my L4 SCA DevOps assignment");
+  res.send("This is the V2 of my submission");
 });
 
 app.listen(3000);
